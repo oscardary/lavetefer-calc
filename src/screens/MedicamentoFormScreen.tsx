d9ed8ea15fc83eeed9400c24b8d108b446f0b6c4
Oskar Rivera <oscardary@gmail.com>
2025-07-24 23:28:56 -0500

    Formulario Se adicionan los campos comentario y activo, adicional se ajusta el scroll, margenes y teclado en el formulario

--- src/screens/MedicamentoFormScreen.tsx
+++ src/screens/MedicamentoFormScreen.tsx
@@ -9,11 +9,14 @@ import {
   SafeAreaView,
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
-  Platform, Keyboard
+  Platform,
+  Keyboard,
+  ScrollView,
+  Switch,
 } from "react-native";
 import { List, Dialog, Portal } from "react-native-paper";
 import { MaterialIcons } from "@expo/vector-icons"; // o Ionicons
-//import { useNavigation } from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native";
 //import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 //import { RootStackParamList } from "../navigation/AppNavigator";
 
@@ -29,10 +32,13 @@ export default function MedicamentoFormScreen() {
   const [posologiaValor, setPosologiaValor] = useState("");
   const [posologiaUnidad, setPosologiaUnidad] = useState("");
   const [comentario, setComentario] = useState("");
-  const [activo, setActivo] = useState("");
+  const [activo, setActivo] = useState(true);
 
   const [visibleConcentracion, setVisibleConcentracion] = useState(false);
   const [visiblePosologia, setVisiblePosologia] = useState(false);
+
+  // dentro del componente MedicamentoFormScreen
+  const navigation = useNavigation();
 
   const guardarMedicamento = async () => {
     if (
@@ -71,7 +77,8 @@ export default function MedicamentoFormScreen() {
       setPosologiaUnidad("");
 
       // Navega de vuelta a la pantalla anterior (opcional)
-      //navigation.goBack();
+      navigation.goBack();
+      //navigation.navigate();
     } catch (error) {
       console.error("Error al guardar medicamento", error);
       Alert.alert("Error", "No se pudo guardar el medicamento");
@@ -84,130 +91,163 @@ export default function MedicamentoFormScreen() {
       behavior={Platform.OS === "ios" ? "padding" : undefined}
     >
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <SafeAreaView style={styles.safeArea}>
-          <View style={{ flex: 1, padding: 16 }}>
-            <Text style={{ fontSize: 20, fontWeight: "bold" }}>
-              Crear / Editar Medicamento
-            </Text>
-            {/* Aquí irán los inputs */}
-            <Text style={styles.label}>Nombre</Text>
-            <TextInput
-              value={nombre}
-              onChangeText={setNombre}
-              placeholder="Ej: Enrofloxacina"
-              style={styles.input}
-            />
-
-            <Text style={styles.label}>Presentación</Text>
-            <TextInput
-              value={presentacion}
-              onChangeText={setPresentacion}
-              placeholder="Ej: tabletas"
-              style={styles.input}
-            />
-
-            <Text style={styles.label}>Concentración</Text>
-            <TextInput
-              value={concentracionValor}
-              onChangeText={setConcentracionValor}
-              placeholder="Ej: 100"
-              keyboardType="decimal-pad"
-              style={styles.input}
-            />
-
-            <Text style={styles.label}>Unidad de concentración</Text>
-            <TouchableOpacity
-              style={styles.dropdownContainer}
-              onPress={() => setVisibleConcentracion(true)}
-            >
-              <Text
-                style={[
-                  styles.dropdownText,
-                  !concentracionUnidad && styles.placeholderText, // aplica estilo si está vacío
-                ]}
-              >
-                {concentracionUnidad || "Selecciona unidad de concentración"}
+        <ScrollView
+          contentContainerStyle={{
+            padding: 5,
+            paddingBottom: 40,
+            flexGrow: 1,
+          }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <SafeAreaView style={styles.safeArea}>
+            <View style={{ flex: 1, padding: 16 }}>
+              <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+                Crear / Editar Medicamento
               </Text>
-              <MaterialIcons name="arrow-drop-down" size={24} color="gray" />
-            </TouchableOpacity>
+              {/* Aquí irán los inputs */}
+              <Text style={styles.label}>Nombre</Text>
+              <TextInput
+                value={nombre}
+                onChangeText={setNombre}
+                placeholder="Ej: Enrofloxacina"
+                style={styles.input}
+              />
 
-            <Portal>
-              <Dialog
-                visible={visibleConcentracion}
-                onDismiss={() => setVisibleConcentracion(false)}
+              <Text style={styles.label}>Presentación</Text>
+              <TextInput
+                value={presentacion}
+                onChangeText={setPresentacion}
+                placeholder="Ej: tabletas"
+                style={styles.input}
+              />
+
+              <Text style={styles.label}>Concentración</Text>
+              <TextInput
+                value={concentracionValor}
+                onChangeText={setConcentracionValor}
+                placeholder="Ej: 100"
+                keyboardType="decimal-pad"
+                style={styles.input}
+              />
+
+              <Text style={styles.label}>Unidad de concentración</Text>
+              <TouchableOpacity
+                style={styles.dropdownContainer}
+                onPress={() => setVisibleConcentracion(true)}
               >
-                <Dialog.Title>Selecciona una unidad</Dialog.Title>
-                <Dialog.Content>
-                  {UNIDADES_CONCENTRACION.map((unidad) => (
-                    <List.Item
-                      key={unidad}
-                      title={unidad}
-                      onPress={() => {
-                        setConcentracionUnidad(unidad);
-                        setVisibleConcentracion(false);
-                      }}
-                    />
-                  ))}
-                </Dialog.Content>
-              </Dialog>
-            </Portal>
+                <Text
+                  style={[
+                    styles.dropdownText,
+                    !concentracionUnidad && styles.placeholderText, // aplica estilo si está vacío
+                  ]}
+                >
+                  {concentracionUnidad || "Selecciona unidad de concentración"}
+                </Text>
+                <MaterialIcons name="arrow-drop-down" size={24} color="gray" />
+              </TouchableOpacity>
 
-            <Text style={styles.label}>Posología</Text>
-            <TextInput
-              value={posologiaValor}
-              onChangeText={setPosologiaValor}
-              placeholder="Ej: 5"
-              keyboardType="decimal-pad"
-              style={styles.input}
-            />
+              <Portal>
+                <Dialog
+                  visible={visibleConcentracion}
+                  onDismiss={() => setVisibleConcentracion(false)}
+                >
+                  <Dialog.Title>Selecciona una unidad</Dialog.Title>
+                  <Dialog.Content>
+                    {UNIDADES_CONCENTRACION.map((unidad) => (
+                      <List.Item
+                        key={unidad}
+                        title={unidad}
+                        onPress={() => {
+                          setConcentracionUnidad(unidad);
+                          setVisibleConcentracion(false);
+                        }}
+                      />
+                    ))}
+                  </Dialog.Content>
+                </Dialog>
+              </Portal>
 
-            <Text style={styles.label}>Unidad de posología</Text>
-            <TouchableOpacity
-              style={styles.dropdownContainer}
-              onPress={() => setVisiblePosologia(true)}
-            >
-              <Text
-                style={[
-                  styles.dropdownText,
-                  !posologiaUnidad && styles.placeholderText, // aplica estilo si está vacío
-                ]}
+              <Text style={styles.label}>Posología</Text>
+              <TextInput
+                value={posologiaValor}
+                onChangeText={setPosologiaValor}
+                placeholder="Ej: 5"
+                keyboardType="decimal-pad"
+                style={styles.input}
+              />
+
+              <Text style={styles.label}>Unidad de posología</Text>
+              <TouchableOpacity
+                style={styles.dropdownContainer}
+                onPress={() => setVisiblePosologia(true)}
               >
-                {posologiaUnidad || "Selecciona unidad de posología"}
-              </Text>
-              <MaterialIcons name="arrow-drop-down" size={24} color="gray" />
-            </TouchableOpacity>
+                <Text
+                  style={[
+                    styles.dropdownText,
+                    !posologiaUnidad && styles.placeholderText, // aplica estilo si está vacío
+                  ]}
+                >
+                  {posologiaUnidad || "Selecciona unidad de posología"}
+                </Text>
+                <MaterialIcons name="arrow-drop-down" size={24} color="gray" />
+              </TouchableOpacity>
 
-            <Portal>
-              <Dialog
-                visible={visiblePosologia}
-                onDismiss={() => setVisiblePosologia(false)}
+              <Portal>
+                <Dialog
+                  visible={visiblePosologia}
+                  onDismiss={() => setVisiblePosologia(false)}
+                >
+                  <Dialog.Title>Selecciona una unidad</Dialog.Title>
+                  <Dialog.Content>
+                    {UNIDADES_POSOLOGIA.map((unidad) => (
+                      <List.Item
+                        key={unidad}
+                        title={unidad}
+                        onPress={() => {
+                          setPosologiaUnidad(unidad);
+                          setVisiblePosologia(false);
+                        }}
+                      />
+                    ))}
+                  </Dialog.Content>
+                </Dialog>
+              </Portal>
+
+              <Text style={styles.label}>Comentario</Text>
+              <TextInput
+                value={comentario}
+                onChangeText={setComentario}
+                placeholder="Ej: Texto descriptivo"
+                style={styles.input}
+              />
+
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  marginTop: 16,
+                }}
               >
-                <Dialog.Title>Selecciona una unidad</Dialog.Title>
-                <Dialog.Content>
-                  {UNIDADES_POSOLOGIA.map((unidad) => (
-                    <List.Item
-                      key={unidad}
-                      title={unidad}
-                      onPress={() => {
-                        setPosologiaUnidad(unidad);
-                        setVisiblePosologia(false);
-                      }}
-                    />
-                  ))}
-                </Dialog.Content>
-              </Dialog>
-            </Portal>
+                <Text style={{ fontSize: 16, marginRight: 12 }}>¿Activo?</Text>
+                <Switch
+                  value={activo}
+                  onValueChange={setActivo}
+                  thumbColor={activo ? "#4CAF50" : "#ccc"}
+                  trackColor={{ false: "#999", true: "#A5D6A7" }}
+                />
+              </View>
 
-            {/*Boton*/}
-            <TouchableOpacity
-              onPress={guardarMedicamento}
-              style={styles.botonGuardar}
-              activeOpacity={0.8}
-            >
-              <Text style={styles.textoBotonGuardar}>Guardar</Text>
-            </TouchableOpacity>
-          </View>
-        </SafeAreaView>
+              {/*Boton*/}
+              <TouchableOpacity
+                onPress={guardarMedicamento}
+                style={styles.botonGuardar}
+                activeOpacity={0.8}
+              >
+                <Text style={styles.textoBotonGuardar}>Guardar</Text>
+              </TouchableOpacity>
+            </View>
+          </SafeAreaView>
+        </ScrollView>
       </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
   );
